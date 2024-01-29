22e86c75affcc378d173764b8b3e1abd924cd86c
vincent <weiheng.liang@gmail.com>
2024-01-28 22:35:29 -0800

    change project name

--- main.go
+++ main.go
@@ -127,7 +127,7 @@ func projectHandler(w http.ResponseWriter, r *http.Request) {
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>MinFileServer</title>
+<title>MinRAGServer</title>
 <link rel="stylesheet" href="/static/style.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 <script>
